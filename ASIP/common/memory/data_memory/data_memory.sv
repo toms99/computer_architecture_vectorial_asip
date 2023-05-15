962d3c9ade769f4bf915c51b6211fc8dae5c3c68
Zeledon Salazar, Kevin <kevin.zeledon.salazar@intel.com>
2023-05-14 23:42:48 -0600

    Memories and their tests

--- ASIP/common/memory/data_memory/data_memory.sv
+++ ASIP/common/memory/data_memory/data_memory.sv
@@ -1,23 +1,32 @@
-module data_memory(input logic clk, write_enable,
-						input logic [31:0] DataAdr, toWrite_data,
-						output logic [31:0] read_data, output logic [31:0] RAM[703:0]);
+module data_memory #(
+    parameter dataSize = 32,
+    parameter addressingSize = 32,
+    parameter memorySize = 704,
+    parameter vecSize = 4
+) (
+    input logic clk, write_enable,
+    input logic [addressingSize-1:0] DataAdr,
+    input logic [vecSize-1:0] [dataSize-1:0] toWrite_data,
+    output logic [vecSize-1:0] [dataSize-1:0] read_data
+);
+    parameter bytes_in_addr = dataSize / 8;
+    parameter bits_to_address_bytes_in_addr = $clog2(bytes_in_addr);
 
-				
-				
+    logic [dataSize-1:0] RAM [memorySize-1:0];
 	initial begin
 		$readmemh("RAM.txt", RAM);
-		for (int i = 0; i < 704 ; i++) begin
-			RAM[i] = 32'b0;
-		end
 	end
-
-	always_ff @(negedge clk) begin
-		read_data = {24'b0, RAM[DataAdr[31:2]]} ; // Alineamiento de la palabra
-	end
-
-	always_ff @(posedge clk) begin
-		if (write_enable) RAM[DataAdr[31:2]] <= toWrite_data;
-		$writememh("RAM.txt",RAM);
-	end
-					
+    
+    always_ff @(posedge clk) begin
+        if (write_enable) begin 
+            for (int i = 0; i < vecSize; i = i + 1)
+                RAM[DataAdr[addressingSize-1:bits_to_address_bytes_in_addr] +
+                    i * bytes_in_addr] <= toWrite_data[i];
+        end else begin 
+            for (int i = 0; i < vecSize; i = i + 1)
+                read_data[i] <= 
+                    RAM[DataAdr[addressingSize-1:
+                                bits_to_address_bytes_in_addr] + i * bytes_in_addr];
+        end
+    end
 endmodule
