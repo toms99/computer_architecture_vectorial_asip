00c64d5c362707b7392e3b2a319d0b5963955dc1
MAraya <marioaray1999@outlook.com>
2023-05-21 20:47:27 -0600

    testing 16 bit instructions

--- ASIP/stage_decode/registerFile/regFile.sv
+++ ASIP/stage_decode/registerFile/regFile.sv
@@ -17,7 +17,7 @@ module regFile #(
     logic [vectorSize-1:0] [registerSize-1:0] vector_reg1Out, vector_reg2Out;
     logic [vectorSize-1:0] [registerSize-1:0] vectorized_scalar_reg1Out, vectorized_scalar_reg2Out;
 
-    scalarRegisterFile #(registerSize, 24, selectionBits) scalarRegisters(
+    scalarRegisterFile #(registerSize, 16, selectionBits) scalarRegisters(
         .clk(clk), .reset(reset),
         .regWrEn(regWrEnSc), .rSel1(rSel1), .rSel2(rSel2),
         .regToWrite(regToWrite), .dataIn(dataIn[0]), // For scalars we will just take in consideration the first element
