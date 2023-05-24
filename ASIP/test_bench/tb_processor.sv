ed30ea04192427c127283ce76ed688d0259749ea
Zeledon Salazar, Kevin <kevin.zeledon.salazar@intel.com>
2023-05-24 13:50:19 -0600

    Test for mmios

--- ASIP/test_bench/tb_processor.sv
+++ ASIP/test_bench/tb_processor.sv
@@ -1,15 +1,26 @@
 module tb_processor();
 
     logic clk, reset;
+    logic mode_xor, mode_rshift, mode_lshift, mode_ecae, mode_dcae;
+    logic mode_mul;
 
-	 processor dut(clk, reset);
+	processor dut(clk, reset, mode_xor, mode_rshift,
+        mode_lshift, mode_ecae, mode_dcae, mode_mul);
 	 
 
     always #5 clk = ~clk;
     initial begin
         clk = 0;
+        mode_xor = 0;
+        mode_rshift = 0;
+        mode_lshift = 0;
+        mode_ecae = 0;
+        mode_dcae = 0;
+        mode_mul = 0;
         reset = 1; # 10 ;
-		  reset = 0;
+		reset = 0;
+        #10;
+        mode_xor = 1;
 
         
     end
