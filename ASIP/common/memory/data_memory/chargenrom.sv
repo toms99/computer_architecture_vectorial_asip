463ad25f62fa3acdcf558a2282e5b72071563be0
Wajib Zaglul <wajib10@gmail.com>
2023-05-24 12:10:27 -0600

    select between 2 images

--- ASIP/common/memory/data_memory/chargenrom.sv
+++ ASIP/common/memory/data_memory/chargenrom.sv
@@ -1,13 +1,21 @@
 module chargenrom( input logic [9:0] x, y, input logic inrect, clk, start, output logic [7:0] pixel);
 
 		logic [7:0] charrom[9999:0];
-		
-		always @(posedge clk or posedge start) begin
+		logic [7:0] img2[9999:0];
+		logic [7:0] temp[9999:0];
+		initial begin
+			$readmemb("RAM.txt", charrom);
+			$readmemb("RAM2.txt", img2);
+		end
+		always @(posedge clk) begin
 			if (start) begin
-				$readmemb("RAM.txt", charrom);
+				temp <= charrom;
+			end
+			else begin
+				temp <= img2;
 			end
 		end
 		
-		assign pixel = inrect ? charrom[100*x + y] : 7'h00;
+		assign pixel = inrect ? temp[100*x + y] : 7'h00;
 		
 endmodule
